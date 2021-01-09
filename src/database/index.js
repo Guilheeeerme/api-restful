2f7b88dae9bfda5dd5980a0e8a043d9a9c709d9e
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-01-08 23:34:12 -0300

    config database

---
+++ src/database/index.js
@@ -0,0 +1,15 @@
+import Sequelize from "sequelize";
+
+import databaseConfig from "../config/database";
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+  }
+}
+
+export default new Database();
